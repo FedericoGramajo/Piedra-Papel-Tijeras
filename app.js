4559496f1c67131e9622d780af5e1600b770d8fb
FedericoGramajo <54726454+FedericoGramajo@users.noreply.github.com>
2022-02-10 10:20:05 -0300

    Cambio de acentos en palabras, stash sin commitear

--- app.js
+++ app.js
@@ -1,123 +1,115 @@
-const juego =()=>{
-     let jPuntaje=0;
-     let cPuntaje=0;
-//Esta funcion inicia el juego cuando das click al 'Jugar!'
-     const iniciarJuego = () =>{
-         const playBtn = document.querySelector(".intro button");
-         const introPantalla = document.querySelector(".intro");
-         const partido = document.querySelector(".partido");
-         const manos = document.querySelectorAll(".manos img");
+const juego = () => {
+    let jPuntaje = 0;
+    let cPuntaje = 0;
+    //Esta funcion inicia el juego cuando das click al 'Jugar!'
+    const iniciarJuego = () => {
+        const playBtn = document.querySelector(".intro button");
+        const introPantalla = document.querySelector(".intro");
+        const partido = document.querySelector(".partido");
+        const manos = document.querySelectorAll(".manos img");
 
-         manos.forEach(manos =>{
-             manos.addEventListener('animationend', function(){
-                 this.style.animation="";
-             })
-         });
+        manos.forEach(manos => {
+            manos.addEventListener("animationend", function () {
+                this.style.animation = "";
+            });
+        });
 
-         playBtn.addEventListener("click", () =>{
-             introPantalla.classList.add("fadeOut");
-             partido.classList.add("fadeIn");
-         })
-     }
+        playBtn.addEventListener("click", () => {
+            introPantalla.classList.add("fadeOut");
+            partido.classList.add("fadeIn");
+        });
+    };
 
-     //Jugar la partida
-     const jugarJuego = () =>{
-         const opciones  = document.querySelectorAll(".opciones button");
-         const manoJugador = document.querySelector(".mano-jugador");
-         const manoComputadora = document.querySelector(".mano-computadora"); 
+    //Jugar la partida
+    const jugarJuego = () => {
+        const opciones = document.querySelectorAll(".opciones button");
+        const manoJugador = document.querySelector(".mano-jugador");
+        const manoComputadora = document.querySelector(".mano-computadora");
 
-         //Opciones de la computador
-         const opcionesComputadora = ['piedra', 'papel', 'tijeras'];
+        //Opciones de la computador
+        const opcionesComputadora = ["piedra", "papel", "tijeras"];
 
-         opciones.forEach(opciones =>{
-            opciones.addEventListener("click", function(){
+        opciones.forEach(opciones => {
+            opciones.addEventListener("click", function () {
+                //Saco un numero random para despues buscarlo en el array opcionesComputadora
+                const numeroComputadora = Math.floor(Math.random() * 3);
+                const computadoraEleccion = opcionesComputadora[numeroComputadora];
 
-        //Saco un numero random para despues buscarlo en el array opcionesComputadora
-         const numeroComputadora = Math.floor(Math.random() * 3);
-         const computadoraEleccion = opcionesComputadora[numeroComputadora];
+                setTimeout(() => {
+                    compararManos(this.textContent.toLowerCase(), computadoraEleccion);
 
-         setTimeout(() =>{
-             
-          compararManos(this.textContent.toLowerCase(), computadoraEleccion);
+                    //Pone las imagenes que elgimos
+                    manoJugador.src = `Imagenes/${this.textContent}.png`;
+                    manoComputadora.src = `Imagenes/${computadoraEleccion}.png`;
+                }, 2000);
 
-          //Pone las imagenes que elgimos
-         manoJugador.src = `Imagenes/${this.textContent}.png`;
-         manoComputadora.src = `Imagenes/${computadoraEleccion}.png`;
+                manoJugador.style.animation = "sacudirJugador 2s ease";
+                manoComputadora.style.animation = "sacudirComputadora 2s ease";
+            });
+        });
+    };
 
-         },2000)
+    const actualizarPuntaje = () => {
+        const puntajeJugador = document.querySelector(".puntaje-jugador p");
+        const puntajeComputadora = document.querySelector(".puntaje-computadora p");
 
-         manoJugador.style.animation = "sacudirJugador 2s ease";
-         manoComputadora.style.animation = "sacudirComputadora 2s ease";
-            })
-        })
+        puntajeJugador.textContent = jPuntaje;
+        puntajeComputadora.textContent = cPuntaje;
+    };
 
-     }
+    const compararManos = (eleccionJugador, eleccionComputadora) => {
+        const ganador = document.querySelector(".ganador");
 
-     const actualizarPuntaje = () =>{
-        const puntajeJugador = document.querySelector(".puntaje-jugador p")
-        const puntajeComputadora = document.querySelector(".puntaje-computadora p")
-
-        puntajeJugador.textContent=jPuntaje;
-        puntajeComputadora.textContent=cPuntaje;
-    }
-
-const compararManos = (eleccionJugador, eleccionComputadora)  =>{
-    const ganador =document.querySelector(".ganador");
-
-    if(eleccionJugador===eleccionComputadora){
-        ganador.textContent= "Es un Empate";
-        return;
-    }
-
-    if(eleccionJugador==="piedra"){
-        if (eleccionComputadora === "tijeras"){
-            ganador.textContent = "Ganaste";
-            jPuntaje++;
-            actualizarPuntaje();
-            return;
-        } else {
-            ganador.textContent = "Perdiste, la Computadora te ganó ):";
-            cPuntaje++;
-            actualizarPuntaje();
+        if (eleccionJugador === eleccionComputadora) {
+            ganador.textContent = "Es un Empate";
             return;
         }
 
-    }
-
-    if(eleccionJugador === "papel"){
-        if (eleccionComputadora === "piedra"){
-            ganador.textContent = "Ganaste";
-            jPuntaje++;
-            actualizarPuntaje();
-            return;
-        } else {
-            ganador.textContent = "Perdiste, la Computadora te ganó ):";
-            cPuntaje++;
-            actualizarPuntaje();
-            return;
+        if (eleccionJugador === "piedra") {
+            if (eleccionComputadora === "tijeras") {
+                ganador.textContent = "Ganaste";
+                jPuntaje++;
+                actualizarPuntaje();
+                return;
+            } else {
+                ganador.textContent = "Perdiste, la Computadora te gano ):";
+                cPuntaje++;
+                actualizarPuntaje();
+                return;
+            }
         }
 
-    }
-
-    if(eleccionJugador==="tijeras"){
-        if (eleccionComputadora === "papel"){
-            ganador.textContent = "Ganaste";
-            jPuntaje++;
-            actualizarPuntaje();
-            return;
-        } else {
-            ganador.textContent = "Perdiste, la Computadora te ganó ):";
-            cPuntaje++;
-            actualizarPuntaje();
-            return;
+        if (eleccionJugador === "papel") {
+            if (eleccionComputadora === "piedra") {
+                ganador.textContent = "Ganaste";
+                jPuntaje++;
+                actualizarPuntaje();
+                return;
+            } else {
+                ganador.textContent = "Perdiste, la Computadora te gano ):";
+                cPuntaje++;
+                actualizarPuntaje();
+                return;
+            }
         }
 
-    }
-}
+        if (eleccionJugador === "tijeras") {
+            if (eleccionComputadora === "papel") {
+                ganador.textContent = "Ganaste";
+                jPuntaje++;
+                actualizarPuntaje();
+                return;
+            } else {
+                ganador.textContent = "Perdiste, la Computadora te gano ):";
+                cPuntaje++;
+                actualizarPuntaje();
+                return;
+            }
+        }
+    };
 
-iniciarJuego();
-jugarJuego();
-}
-
+    iniciarJuego();
+    jugarJuego();
+};
 
 juego();
